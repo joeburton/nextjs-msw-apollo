7cead63f1ae29ab143d2289b7d8e2eb2dc907410
Joe Burton <joeburton@gmail.com>
2021-12-16 11:44:33 +0000

    exploring msw logs

--- jest.setup.js
+++ jest.setup.js
@@ -1,19 +1,20 @@
-import '@testing-library/jest-dom/extend-expect'
-import 'isomorphic-unfetch'
-import { server } from './mocks/server'
+import '@testing-library/jest-dom/extend-expect';
+import 'isomorphic-unfetch';
+import { server } from './mocks/server';
 
 beforeAll(() => {
   // Enable the mocking in tests.
-  server.listen()
-  server.printHandlers()
-})
+  server.listen();
+  // uncomment to view msw logs
+  // server.printHandlers();
+});
 
 afterEach(() => {
   // Reset any runtime handlers tests may use.
-  server.resetHandlers()
-})
+  server.resetHandlers();
+});
 
 afterAll(() => {
   // Clean up once the tests are done.
-  server.close()
-})
+  server.close();
+});
